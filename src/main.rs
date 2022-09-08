825cbaa0204d7c3337935bbba9a5ebc0d280aa68
seyoatda <seyoatda@foxmail.com>
2022-09-08 22:49:11 +0800

    success

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,10 @@ fn main() {
     when_keys_pressed(&[Vk::CapsLock, Vk::KeyH], || {
         Vk::LeftArrow.press();  
     });
+    
     bind_key_set(&[Vk::CapsLock,Vk::KeyJ], Vk::DownArrow);
-
+    bind_key_set(&[Vk::CapsLock,Vk::KeyK],Vk::UpArrow);
+    bind_key_set(&[Vk::CapsLock,Vk::KeyL], Vk::RightArrow);
+    //bind_key_set(&[Vk::CapsLock,Vk::], )
     listen_event();
 }
